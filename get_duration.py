72176a7d4badf6b0a6aed49b0ae10d28bdbc028d
Pavel Zaikin <zaikinpv@gmail.com>
2018-04-19 06:53:26 +0500

    Add error printing option
    Some impruvements. Add an error option, to print only corrupted files.

--- get_duration.py
+++ get_duration.py
@@ -5,21 +5,25 @@ from ffprobe3 import FFProbe
 total_duration = 0.0
 cnt = 0
 verbose = '--v' in sys.argv
+only_errors = '--errors' in sys.argv
 
 if len(sys.argv)==1:
     print(
 """Usage: python get_duration path [--v] 
 
     --v verbose mode (for debugging purpose)
+    --errors print file name when an error occurred while extracting duration time (corrupted video?)
     
     Recursively pathing through the directory and calculate total duration of mp4,mov,avi files.
     Depends on ffprobe, must be accesible in current dir.
     """)
+    exit()
 
 def checkDir(dirname):
     global total_duration
     global cnt
     global verbose
+    global only_errors
     for f in os.scandir(dirname):
         if f.is_dir():
             checkDir(f.path)
@@ -29,7 +33,7 @@ def checkDir(dirname):
                 try:
                     metadata=FFProbe(f.path)
                 except:
-                    if verbose:
+                    if verbose or only_errors:
                         print('Error in file',f.path)
                     continue
                 duration_vs = 0.0
